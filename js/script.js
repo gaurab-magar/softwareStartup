126567ebe653467f79f7d7bd87b9198bdba2f43d
HP <gdikshan10@gmail.com>
2023-12-14 21:45:58 +0545

    i have added successfully

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function userScroll(){
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll',() => {
+        if(window.scrollY >= 50){
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('border-bottom');
+            navbar.classList.add('border-primary')
+            navbar.classList.add('navbar-sticky');
+        }else{
+            navbar.classList.remove('bg-dark');
+            navbar.classList.remove('border-bottom');
+            navbar.classList.remove('border-secondary');
+            navbar.classList.remove('navbar-sticky');
+        }
+    })
+}
+
+
+
+const checkReplace = document.querySelector('.replace-me');
+if(checkReplace !== null){
+    const replace = new ReplaceMe(checkReplace,{
+        animation: 'animated fadeIn',
+        speed: 2000,
+        seperator:',',
+        loop: 'infinite',
+        autorun: true
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', userScroll);
+
+const videoBtn = document.querySelector('.video-btn')
+const videoModal = document.querySelector('#videoModal')
+const video = document.querySelector('#video')
+let videoSrc;
+if(videoBtn !== null){
+    videoBtn.addEventListener('click',()=>{
+        videoSrc = videoBtn.getAttribute('data-bs-src');
+    });
+}
+if(videoModal !== null){
+    videoModal.addEventListener('shown.bs.modal',()=>{
+        video.setAttribute(
+            'src',
+            videoSrc + '?autoplay=1;modestbranding=1;showInfo=0'
+            );
+    })
+    videoModal.addEventListener('hide.bs.modal',()=>[
+        video.setAttribute('src',videoSrc)
+    ])
+}
